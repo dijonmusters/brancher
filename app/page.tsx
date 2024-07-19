5674e14a5a5e9054b712b08b2f1c7fbe74d74baa
Jon Meyers <jonathon.d.meyers@gmail.com>
2024-07-19 17:00:34 +1000

    again

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
 
 export default async function Home() {
-  const cookieJar = cookies()
+  const cookieJar = cookies();
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
@@ -13,7 +13,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <pre>{JSON.stringify({data, cookieJar}, null, 2)}</pre>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
   );
 }
